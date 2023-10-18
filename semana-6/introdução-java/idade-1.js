e7cc6a8500b717d6c7c8c10c35fb5af3ac524496
Bernardo Dartora <bernardintd@gmail.com>
2023-10-18 16:56:41 -0300

    atv-pronta

---
+++ semana-6/introdução-java/idade-1.js
@@ -0,0 +1,8 @@
+let Ano = 98
+let mes = 3
+let dia = 1
+
+let idade 
+idade = (Ano * 365) + (mes * 30) + dia
+
+console.log ("sua idade contadas em dias é: ", idade)
--- semana-6/introdução-java/media-1.js
+++ semana-6/introdução-java/media-1.js
@@ -1,20 +1,21 @@
 // Notas e pesos
 let nota1 = 5.5;
-let nota2 = 3.0; 
+let nota2 = 7.0; 
 let nota3 = 8.2; 
 
 let peso1 = 2;
 let peso2 = 3;
 let peso3 = 5; 
 
-// Cálculo da média ponderada
-let somaProdutos = nota1 * peso1 + nota2 * peso2 + nota3 * peso3;
-let somaPesos = peso1 + peso2 + peso3;
-let mediaPonderada = somaProdutos / somaPesos;
+let media 
 
-// Verificar se o aluno passou ou reprovou
-if (mediaPonderada >= 7) {
-  console.log("O aluno passou com média ponderada de", mediaPonderada);
+media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3)
+
+console.log (media)
+
+if (media >= 7)  {
+  console.log ("Parabéns! vc foi muito bem")
+
 } else {
-  console.log("O aluno reprovou com média ponderada de", mediaPonderada);
+  console.log ("precisa estudar muito mais seu banana")
 }
