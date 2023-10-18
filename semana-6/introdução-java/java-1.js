8ebcb14b995e885f00f5a8952e273319513d3245
Bernardo Dartora <bernardintd@gmail.com>
2023-10-18 16:19:04 -0300

    teste

--- semana-6/introdução-java/java-1.js
+++ semana-6/introdução-java/java-1.js
@@ -1,6 +1,16 @@
-let notaUm = 2
+// Notas e pesos
+let nota1 = 80;
+let nota2 = 90;
+let nota3 = 75;
 
-let notaDois = 3
+let peso1 = 0.3;
+let peso2 = 0.4;
+let peso3 = 0.3;
 
-let notaTres = 5
+// Cálculo da média ponderada
+let somaProdutos = nota1 * peso1 + nota2 * peso2 + nota3 * peso3;
+let somaPesos = peso1 + peso2 + peso3;
+let mediaPonderada = somaProdutos / somaPesos;
 
+// Exibir a média ponderada
+console.log("A média ponderada das notas é:", mediaPonderada);
