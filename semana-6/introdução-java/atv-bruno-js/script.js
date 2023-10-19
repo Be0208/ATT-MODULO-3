4e579d8fd4589317f08121692ee6652e9eebf189
Bernardo Dartora <bernardintd@gmail.com>
2023-10-19 17:17:16 -0300

    FEITOO

--- semana-6/introdução-java/atv-bruno-js/script.js
+++ semana-6/introdução-java/atv-bruno-js/script.js
@@ -71,4 +71,126 @@ const nota2 = 5.3
 const nota3 = 3.3
 
 const media = ((nota1 + nota2 + nota3) / 3  )
-document.write(`A aluna ${nome} ficou com media ${media.toFixed(1)}`)
+document.write(`<h3>A aluna ${nome} ficou com media ${media.toFixed(1)} <br> </h3>`)
+
+
+// 6. Desenvolva um algoritmo que armazene o valor 10 em uma variável
+// A e o valor 20 em uma variável B. A seguir (utilizando apenas
+// atribuições entre variáveis) troque os seus conteúdos fazendo com
+// que o valor que está em A passe para B e vice-versa. Ao final,
+// escrever os valores que ficaram armazenados nas variáveis.
+
+let a = 10
+let b = 20
+
+let c = a 
+a = b 
+b = c
+
+document.write(`A: ${a}  B: ${b}`)
+
+
+
+
+// 7. Desenvolva um algoritmo para ler o número total de eleitores de um
+// município, o número de votos brancos, nulos e válidos. Calcular e
+// escrever o percentual que cada um representa em relação ao total
+// de eleitores.
+
+let votosTotal = 456
+let votosBranco = 45
+let votosNulos = 87
+
+const brencoPerCent = (votosBranco/votosTotal) * 100
+const nuloPerCent = (votosNulos/votosTotal) * 100
+const votosValidos = (((votosTotal - votosNulos - votosBranco) / votosTotal) * 100)
+
+document.write(`O perncentual de votos validos é ${votosValidos.toFixed(2)}%, percentual de votos nulos é ${votosNulos.toFixed(2)}% e o percentual de votos brancos é ${brencoPerCent.toFixed(2)}% <br> <br>`)
+
+
+
+
+
+
+// 8. Desenvolva um algoritmo para ler dois valores e imprimir uma das
+// três mensagens a seguir:
+// a. ‘Números iguais’, caso os números sejam iguais;
+// b. ‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+// c. ‘Segundo maior’, caso o segundo seja maior que o primeiro.
+
+let valor1 = 50
+let valor2 = 150
+
+if(valor1 === valor2) {
+    document.write(`Números iguais!`)
+} else if (valor1 > valor2) {
+    document.write(`O primeiro número é maior! <br> <br> `)
+} else {
+    document.write(`O segundo número é maior! <br> <br> `)
+}
+
+
+
+
+
+// 9. As maçãs desta estação custam R$0,30 se forem compradas
+// menos do que uma dúzia, e R$0,25 se forem compradas pelo menos
+// doze. Desenvolva um algoritmo que leia o número de maçãs
+// compradas, calcule e escreva o valor total da compra.
+
+const macas = 20
+
+if (macas < 12 ) {
+    const total = macas * 0.3
+    document.write(`Valor total da compra R$${total.toFixed(2)} <br> <br>`)
+} else {
+    const total = macas * 0.25
+    document.write(`Valor total da compra R$${total.toFixed(2)} <br> <br>`)
+}
+
+
+
+// 10. Escreva um algoritmo que tenha como valores de entradas o nome
+// e idade do usuário e imprima no terminal o nome, a idade e o ano
+// de nascimento do usuário. Ex: “Nome: Pedro, Idade: 22 anos, nasceu
+// em 2000”.
+// OBS: Pegue o ano atual como base
+
+const name = "bernardo"
+const idadi = 17
+document.write(`nome: ${name} idade: ${idadi} ano que nasceu: ${2023 - idadi}  <br> <br>`)
+
+
+
+
+
+// 11. Crie um algoritmo que armazene um número inteiro positivo, e gere
+// um alerta com as seguintes mensagens:
+// a. “Número é par!”, se o valor armazenado for par;
+// b. “Número é impar!”, se o valor armazenado for ímpar;
+
+const numero = 10 
+
+if (numero % 2 === 0) {
+    document.write(`O número ${numero} é par! <br> <br>`)
+} else {
+    document.write(`O número ${numero} é impar! <br> <br>`)
+}
+
+
+
+
+
+// 12. Escreva um algoritmo que armazene o ano atual e o ano de
+// nascimento de uma pessoa. Escrever uma mensagem no console
+// que diga se ela poderá ou não votar este ano (não é necessário
+// considerar o mês em que a pessoa nasceu).
+
+const anoAtual = 2023
+const anoNascimento = 2006
+
+if (anoAtual - anoNascimento >= 16) {
+    document.write(`Voce pode votar esse ano :)`)
+} else {
+    document.write(`Voce não pode votar esse ano :(`)
+}
