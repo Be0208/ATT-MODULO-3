f2b686bdbadc05f51e8e552c22c6af8879098b14
Bernardo Dartora <bernardintd@gmail.com>
2023-10-19 14:21:20 -0300

    calc

---
+++ semana-6/introdução-java/calc-1.js
@@ -0,0 +1,11 @@
+let num1 = 8
+let num2 = 9
+
+let add = (num1 + num2)
+let sub = (num1 - num2)
+let mul = (num1 * num2)
+let div = (num1 / num2)
+
+resultado = add
+
+console.log("Concluindo: ", resultado)
